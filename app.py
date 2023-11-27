1ff7396c9d8f747bca39f2663da64c577838bfcd
guilhermeonrails <guilhermelimaruby@gmail.com>
2023-11-27 14:00:58 -0300

    aula_2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -8,9 +10,25 @@ print("""
 """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida)
+
+def finalizar_app():
+    os.system('cls')
+    # os.system('clear') no mac
+    print('Finalizando o app\n')
+
+if opcao_escolhida == 1: 
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2: 
+    print('Listar restaurantes')
+elif opcao_escolhida == 3: 
+    print('Ativar restaurante')
+else: 
+    finalizar_app()
+
+
